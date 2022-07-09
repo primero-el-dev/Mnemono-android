f67b578f8bc2c0330a9549a2e8aeca84ae023175
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2022-07-09 15:16:35 +0200

    Sort props

--- app/src/main/java/com/primeroeldev/mnemono/entity/Game.kt
+++ app/src/main/java/com/primeroeldev/mnemono/entity/Game.kt
@@ -20,11 +20,11 @@ enum class GameStatus
 class Game
 {
     public var id: Int? = null
-    public lateinit var type: GameType
-    public lateinit var createdAt: LocalDateTime
-    public lateinit var duration: LocalDateTime
     public var status: GameStatus = GameStatus.NOT_STARTED_STATUS
     public var includedInStatistics: Boolean = false
     public var correctAnswersCount: Int = 0
     public var allAnswersCount: Int = 0
+    public lateinit var type: GameType
+    public lateinit var createdAt: LocalDateTime
+    public lateinit var duration: LocalDateTime
 }
