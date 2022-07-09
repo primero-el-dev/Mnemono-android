cc3e6a10aeac1ec0e09d64db09c5af0027edf1ba
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2022-07-09 15:17:18 +0200

    Rename

--- app/src/main/java/com/primeroeldev/mnemono/entity/Game.kt
+++ app/src/main/java/com/primeroeldev/mnemono/entity/Game.kt
@@ -5,22 +5,22 @@ import java.time.LocalDateTime
 
 enum class GameType
 {
-    NUMBERS_TYPE,
-    WORDS_TYPE,
-    CARDS_TYPE,
+    NUMBERS,
+    WORDS,
+    CARDS,
 }
 
 enum class GameStatus
 {
-    NOT_STARTED_STATUS,
-    JUST_STARTED_STATUS,
-    FINISHED_STATUS,
+    NOT_STARTED,
+    JUST_STARTED,
+    FINISHED,
 }
 
 class Game
 {
     public var id: Int? = null
-    public var status: GameStatus = GameStatus.NOT_STARTED_STATUS
+    public var status: GameStatus = GameStatus.NOT_STARTED
     public var includedInStatistics: Boolean = false
     public var correctAnswersCount: Int = 0
     public var allAnswersCount: Int = 0
