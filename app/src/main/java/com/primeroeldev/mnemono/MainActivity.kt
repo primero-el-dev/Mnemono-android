732e202e793120f2bbcd459c57d00e7e4f8794a0
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2022-07-17 22:39:44 +0200

    Finish game start display

--- app/src/main/java/com/primeroeldev/mnemono/MainActivity.kt
+++ app/src/main/java/com/primeroeldev/mnemono/MainActivity.kt
@@ -1,24 +1,24 @@
 package com.primeroeldev.mnemono
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.primeroeldev.mnemono.game.Game
-import com.primeroeldev.mnemono.general.readInstanceProperty
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.primeroeldev.mnemono.activity.GameStartActivity
 
 
 class MainActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
-        val game = Game()
-//        game.status = "Status"
-//        val value = readInstanceProperty<Any>(game, "status")
-//
-//        val v = 1
-//        val s = (v.javaClass) value
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    fun startGame(view: View)
+    {
+        val intent = Intent(this, GameStartActivity::class.java)
+
+        startActivity(intent)
     }
 }
